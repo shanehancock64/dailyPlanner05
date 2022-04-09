fc237bef02ec8d222c604a180e24d6c824ad36f6
Michael Hancock <shanehancock64@live.com>
2022-04-09 16:20:04 -0700

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,115 @@
+// Current Date and Time
+var now = moment().format("MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(now);
+
+// current time
+var hourNow = moment().hour();
+
+// Checks current time and changes color blocks
+function hourlyTrack() {
+  // Hour 9
+  if (hourNow > parseInt($("#hour9").children("div").attr("data-hours"))) {
+    $("#hour9").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour9").children("div").attr("data-hours"))
+  ) {
+    $("#hour9").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour9").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 10
+  if (hourNow > parseInt($("#hour10").children("div").attr("data-hours"))) {
+    $("#hour10").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour10").children("div").attr("data-hours"))
+  ) {
+    $("#hour10").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour10").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 11
+  if (hourNow > parseInt($("#hour11").children("div").attr("data-hours"))) {
+    $("#hour11").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour9").children("div").attr("data-hours"))
+  ) {
+    $("#hour11").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour11").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 12
+  if (hourNow > parseInt($("#hour12").children("div").attr("data-hours"))) {
+    $("#hour12").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour12").children("div").attr("data-hours"))
+  ) {
+    $("#hour12").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour12").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 13
+  if (hourNow > parseInt($("#hour13").children("div").attr("data-hours"))) {
+    $("#hour13").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour13").children("div").attr("data-hours"))
+  ) {
+    $("#hour13").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour13").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 14
+  if (hourNow > parseInt($("#hour14").children("div").attr("data-hours"))) {
+    $("#hour14").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour14").children("div").attr("data-hours"))
+  ) {
+    $("#hour14").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour14").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 15
+  if (hourNow > parseInt($("#hour15").children("div").attr("data-hours"))) {
+    $("#hour15").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour15").children("div").attr("data-hours"))
+  ) {
+    $("#hour15").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour15").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 16
+  if (hourNow > parseInt($("#hour16").children("div").attr("data-hours"))) {
+    $("#hour16").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour16").children("div").attr("data-hours"))
+  ) {
+    $("#hour16").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour16").children("div").siblings("textarea").addClass("present");
+  }
+  // Hour 17
+  if (hourNow > parseInt($("#hour17").children("div").attr("data-hours"))) {
+    $("#hour17").children("div").siblings("textarea").addClass("past");
+  } else if (
+    hourNow < parseInt($("#hour17").children("div").attr("data-hours"))
+  ) {
+    $("#hour17").children("div").siblings("textarea").addClass("future");
+  } else {
+    $("#hour17").children("div").siblings("textarea").addClass("present");
+  }
+}
+// Start Function
+hourlyTrack();
+
+// Save to local storage
+var save_button = document.getElementById('save')
+save_button.onclick = saveData;
+
+function saveData(){
+  var input = document.getElementById('input');
+  localStorage.setItem('saved', input.value);
+  var storedValue = localStorage.getItem('saved');
+}
+$(document).ready(function(storedValue){
+  localStorage.getItem('saved')
+});
---
+++ README.md
@@ -0,0 +1,11 @@
+For this assignment I had to create a daily planner that had the folllowing:
+1. The current day is displayed at the top of the calendar.
+2. Presented with timeblocks for standard business hours.
+3. Each timeblock is color coded to indicate whether it is in the past, present, or future.
+4. I can enter an event.
+5. The text for that event is saved in local storage. 
+
+I went with a more basic If/ELSE approach to get the time blocks correct to keep it from getting too complicated. 
+
+screen shot of it deployed: 
+<img src="./Assets/Screen Shot 2022-04-09 at 3.36.22 PM.png"></img>
